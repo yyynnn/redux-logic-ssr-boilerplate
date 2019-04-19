9d130245d206a3817612936c44b1e361e3a03523
Sergio <snikolaev@mtsbank.ru>
2019-04-19 13:24:32 +0300

    initial commit

--- src/server/helpers/sizes.js
+++ src/server/helpers/sizes.js
@@ -1,23 +1,22 @@
 import MobileDetect from 'mobile-detect'
-import { theme } from 'mtsbankui'
 
 export const getSizesFallback = userAgent => {
   const md = new MobileDetect(userAgent)
 
-  if (md.mobile()) {
-    return {
-      fallbackWidth: theme.breakpoints.md,
-      fallbackHeight: 640
-    }
-  } else if (md.tablet()) {
-    return {
-      fallbackWidth: theme.breakpoints.lg,
-      fallbackHeight: 1024
-    }
-  }
+  // if (md.mobile()) {
+  //   return {
+  //     fallbackWidth: theme.breakpoints.md,
+  //     fallbackHeight: 640
+  //   }
+  // } else if (md.tablet()) {
+  //   return {
+  //     fallbackWidth: theme.breakpoints.lg,
+  //     fallbackHeight: 1024
+  //   }
+  // }
 
   return {
-    fallbackWidth: theme.breakpoints.xl,
+    fallbackWidth: 1270,
     fallbackHeight: 700
   }
 }
